7d078481eb3ac88c1220f9483f36b5fb2809b26c
Zorxh <Zorxh96@gmail.com>
2021-04-14 20:00:40 +0200

    Updated docker-compose with the Discount API project.

---
+++ src/Services/Discount/API/Discount.Api/Extensions/HostExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Discount.Api.Extensions
+{
+    public static class HostExtensions
+    {
+        public static void MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+
+        }
+    }
+}
