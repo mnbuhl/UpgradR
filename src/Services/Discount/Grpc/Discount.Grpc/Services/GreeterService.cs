52b90ce9bd35a68f1d51768b400ac9f7bdaa7ae3
Zorxh <Zorxh96@gmail.com>
2021-04-16 14:43:24 +0200

    Added gRPC Discount service to the project.

---
+++ src/Services/Discount/Grpc/Discount.Grpc/Services/GreeterService.cs
@@ -0,0 +1,24 @@
+using Discount.Grpc.Protos;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
