ad7459e1ce7dc87a36067a07d75aa72545a7c799
Zorxh <Zorxh96@gmail.com>
2021-04-21 18:40:31 +0200

    Added Interfaces for the repository as well as Email service with model classes

---
+++ src/Services/Order/Order.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using Order.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Order.Application.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<IReadOnlyList<T>> GetAllAsync(List<Expression<Func<T, object>>> includes = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+            List<Expression<Func<T, object>>> includes = null);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ src/Services/Order/Order.Application/Interfaces/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Order.Application.Interfaces
+{
+    public interface IOrderRepository : IGenericRepository<Domain.Entities.Order>
+    {
+        Task<IEnumerable<Domain.Entities.Order>> GetOrdersByUsername(string userName);
+    }
+}
