d76b58cdcea32564ec6ba9d0600b571c1dd9430e
Zorxh <Zorxh96@gmail.com>
2021-04-22 23:16:32 +0200

    Added EmailService class

---
+++ src/Services/Order/Order.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Order.Application.Interfaces;
+using Order.Application.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace Order.Infrastructure.Email
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings _settings;
+        private readonly ILogger<EmailService> _logger;
+
+        public EmailService(IOptions<EmailSettings> settings, ILogger<EmailService> logger)
+        {
+            _settings = settings.Value;
+            _logger = logger;
+        }
+
+        public async Task<bool> SendEmail(Application.Models.Email email)
+        {
+            var client = new SendGridClient(_settings.ApiKey);
+
+            string subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            string emailBody = email.Body;
+
+            var from = new EmailAddress
+            {
+                Email = _settings.FromAddress,
+                Name = _settings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            _logger.LogInformation("Email sent.");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+                return true;
+
+            _logger.LogError("Email sending failed.");
+            return false;
+        }
+    }
+}
